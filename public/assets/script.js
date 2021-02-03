9d5438ba7bdf922bfef0a0cd179e2ad3e5b3f25c
James Gould <james.gould@kincentric.com>
2021-02-03 13:58:07 -0600

    Initial conversion to html5 up template

--- public/assets/script.js
+++
@@ -1,7 +0,0 @@
-$("#townesLink")
-  .on("mouseover", function () {
-    $("#about-img").attr("src", "./assets/img/townes.jpg");
-  })
-  .on("mouseleave", function () {
-    $("#about-img").attr("src", "./assets/img/jcg_piano.jpg");
-  });
