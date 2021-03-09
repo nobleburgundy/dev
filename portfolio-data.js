eca477ae7202f021febe0bf5155aa63dfb31ecf6
James Gould <nobleburgundy@gmail.com>
2021-03-09 12:47:50 -0600

    Added some projects to the portfolio

--- portfolio-data.js
+++ portfolio-data.js
@@ -1,5 +1,13 @@
 const portfolio = [
   {
+    title: "Canopy",
+    description: "A basic employee information table with search and filtering built in React.",
+    thumbnail: "canopy.png",
+    technologies: ["React"],
+    github: "https://nobleburgundy.github.com/canopy/",
+    link: "https://nobleburgundy.github.io/canopy/",
+  },
+  {
     title: "Orbit",
     description:
       "A full stack MVP app where users can find, create, or navigate clubs/social groups or events taking place in digital spaces.",
@@ -7,6 +15,22 @@ const portfolio = [
     technologies: ["Node", "Express", "Handlebars", "MySQL", "Passport"],
     github: "https://nobleburgundy.github.com/orbit/",
     link: "https://wereinorbit.herokuapp.com/",
+  },
+  {
+    title: "Mongo Fitness",
+    description: "A fitness tracking application using MongoDB, Express, and Node.",
+    thumbnail: "mongo_fitness.png",
+    technologies: ["Node", "Express", "Handlebars", "MongoDB"],
+    github: "https://nobleburgundy.github.com/mongo-fitness/",
+    link: "https://james-fitness-tracker.herokuapp.com/",
+  },
+  {
+    title: "PWA Budget",
+    description: "A simple PWA for tracking budget online and off..",
+    thumbnail: "pwa_budget.png",
+    technologies: ["Service Workers", "Webpack", "Babel", "Compression library", "IndexedDb"],
+    github: "https://github.com/nobleburgundy/pwa-budget/",
+    link: "https://jcg-pwa-budget.herokuapp.com/",
   },
   {
     title: "Burger Bonanza",
