0ad120d29f59d7a0a47903e7ed91d99dd6af33f4
James Gould <nobleburgundy@gmail.com>
2022-11-07 10:36:27 -0600

    Updated job experience and portfolio.

---
+++ jobs-data.js
@@ -0,0 +1,31 @@
+const jobs = [
+  {
+    jobTitle: "Software Engineer",
+    company: "Code42",
+    startDate: "April 2021",
+    endDate: "Nov 2022",
+    reason: "Laid off due to company reduction",
+    description: "Full stack engineer on a development team responsible for building and testing key company applications.",
+    techStack: ["Angular", "dotnet", "MongoDB", "Java"]
+  },
+  {
+    jobTitle: "Test Automation Architect",
+    company: "Kincentric",
+    startDate: "April 2021",
+    endDate: "November 2022",
+    reason: "Laid off due to company reduction",
+    description: "Lead a group of test automation engineers in building and maintaing a test automation framework with Selenium.",
+    techStack: ["C#", "Gerhkin", "Selenium"]
+  },
+  {
+    jobTitle: "Senior Software Engineer in Test",
+    company: "Target",
+    startDate: "April 2021",
+    endDate: "November 2022",
+    reason: "Laid off due to company reduction",
+    description: "Full stack engineer on a development team responsible for building and testing key company applications.",
+    techStack: ["Angular", "dotnet", "MongoDB", "Java"]
+  },
+];
+
+module.exports = jobs;
