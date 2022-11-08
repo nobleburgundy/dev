61940954944d14d719b86451818eb2f89c07625a
James Gould <nobleburgundy@gmail.com>
2022-11-08 10:05:35 -0600

    Job info updates.

--- jobs-data.js
+++ jobs-data.js
@@ -4,27 +4,27 @@ const jobs = [
     company: "Code42",
     startDate: "April 2021",
     endDate: "Nov 2022",
-    reason: "Laid off due to company reduction",
+    reason: "Laid off due to company reduction.",
     description: "Full stack engineer on a development team responsible for building and testing key company applications.",
-    techStack: ["Angular", "dotnet", "MongoDB", "Java"]
+    techStack: ["Angular", "C#", "MongoDB", "Node", "Java", "Docker", "Jenkins"]
   },
   {
     jobTitle: "IT Test Lead",
     company: "Kincentric",
-    startDate: "April 2021",
-    endDate: "November 2022",
-    reason: "Laid off due to company reduction",
-    description: "Led a group of test automation engineers in building and maintaing a test automation framework with Selenium.",
-    techStack: ["C#", "Gerhkin", "Selenium"]
+    startDate: "Dec 2018",
+    endDate: "Nov 2021",
+    reason: "Left for a Software Engineering role at Code42.",
+    description: "Led a group of test automation engineers in building and maintaing a test automation framework with Selenium and Dotnet.",
+    techStack: ["C#", "Gerhkin", "Selenium", "Azure"]
   },
   {
     jobTitle: "Senior Software Engineer in Test",
     company: "Target",
-    startDate: "April 2021",
-    endDate: "November 2022",
-    reason: "Laid off due to company reduction",
-    description: "Full stack engineer on a development team responsible for building and testing key company applications.",
-    techStack: ["Angular", "dotnet", "MongoDB", "Java"]
+    startDate: "July 2015",
+    endDate: "Nov 2018",
+    reason: "Left to lead a group fo test engineers at Kincentric.",
+    description: "Developed test automation framekwork for an internal CMS tool driving Target.com and email marketing teams.",
+    techStack: ["React", "PHP", "MongoDB", "Selenium", "Docker"]
   },
 ];
 
